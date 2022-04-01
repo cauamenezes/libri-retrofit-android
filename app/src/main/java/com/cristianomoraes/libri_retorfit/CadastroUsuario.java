b84607f400a08f41dd598a86698c976f0f13a962
älexgomesz <79976791+cauamenezes@users.noreply.github.com>
2022-04-01 11:56:48 -0700

    cadastro de usuário sendo feito através da comunicação com a API

--- app/src/main/java/com/cristianomoraes/libri_retorfit/CadastroUsuario.java
+++ app/src/main/java/com/cristianomoraes/libri_retorfit/CadastroUsuario.java
@@ -3,11 +3,18 @@ package com.cristianomoraes.libri_retorfit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cristianomoraes.libri_retorfit.model.Usuario;
+import com.cristianomoraes.libri_retorfit.remote.APIUtil;
 import com.cristianomoraes.libri_retorfit.remote.RouterInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CadastroUsuario extends AppCompatActivity {
 
@@ -49,7 +56,30 @@ public class CadastroUsuario extends AppCompatActivity {
             usuario.setSenha(txtSenha.getText().toString());
 
             /** PASSAR OS DADOS PARA A APIREST **/
-            
+            routerInterface = APIUtil.getUsuarioInterface();
+            addUsuario(usuario);
+        });
+
+    } //FIM DO MÉTODO onCreate
+
+    /** IMPLEMENTAÇÃO DO MÉTODO DE Call addUsuario **/
+    public void addUsuario(Usuario usuario) {
+
+        Call<Usuario> call = routerInterface.addUsuario(usuario);
+
+        call.enqueue(new Callback<Usuario>() {
+
+            @Override
+            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                Toast.makeText(CadastroUsuario.this, "USUÁRIO INSERIDO COM SUCESSO",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<Usuario> call, Throwable t) {
+
+                Log.d("ERRO-API", t.getMessage());
+            }
         });
     }
-}
+}//FIM DA CLASSE
